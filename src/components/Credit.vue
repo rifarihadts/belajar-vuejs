f1c4868af310c57ba1da0ee229782be681e063dd
Ridho Fariha <ridho@majaindo.com>
2020-11-13 15:20:53 +0700

    fixed design

--- src/components/Credit.vue
+++ src/components/Credit.vue
@@ -6,7 +6,7 @@
 
     <div class="form-group">
       <label for="nomor_kartu">Nomor Kartu</label>
-      <div class="input-group">
+      <div class="input-group" style="position: relative">
         <input
           id="nomor_kartu"
           name="nomor_kartu"
@@ -15,63 +15,84 @@
           class="form-control"
           :maxlength="16"
           @keypress="isNumber($event)"
+          :class="{ 'error': !nomor_kartu_valid, 'success' : nomor_kartu_valid }"
         />
-        <div class="input-group-append"></div>
+        <div class="success-icon" v-if="nomor_kartu_valid">
+          <i class="fas fa-check-circle"></i>
+        </div>
+        <!-- <div class="input-group-append"></div> -->
       </div>
-      <span class="error_message">{{ cc_message }}</span>
     </div>
     <div class="row">
       <div class="col-sm-8">
         <div class="form-group">
           <label><span class="hidden-xs">Valid Date</span></label>
           <div class="input-group">
-            <input
-              @keypress="isNumber($event)"
-              :maxlength="2"
-              v-model="mm"
-              placeholder="MM"
-              name=""
-              class="form-control"
-              required
-            />
-            <input
-              @keypress="isNumber($event)"
-              :maxlength="2"
-              v-model="yy"
-              placeholder="YY"
-              name=""
-              class="form-control"
-              required
-            />
-            </div>
-            <div>
-              <span class="error_message">{{ mm_message }}</span>
-              <span class="error_message" :style="mm_valid? '' : 'padding-left:80px'">{{ yy_message }}</span>
-            </div>
+            <span style="width:25%;position:relative;display:inline-block">
+              <input
+                @keypress="isNumber($event)"
+                :maxlength="2"
+                v-model="mm"
+                placeholder="MM"
+                name="mm"
+                class="form-control"
+                :class="{ 'error': !mm_valid, 'success' : mm_valid}"
+                required
+              />
+              <div class="success-icon" v-if="mm_valid">
+                <i class="fas fa-check-circle"></i>
+              </div>
+            </span>
+            <span style="width:25%;position:relative;display:inline-block">
+              <input
+                @keypress="isNumber($event)"
+                :maxlength="2"
+                v-model="yy"
+                placeholder="YY"
+                name="yy"
+                class="form-control"
+                :class="{ 'error': !yy_valid, 'success' : yy_valid}"
+                required
+              />
+              <div class="success-icon" v-if="yy_valid">
+                <i class="fas fa-check-circle"></i>
+              </div>
+            </span>
+          </div>
+          
         </div>
       </div>
       <div class="col-sm-4">
         <div class="form-group mb-4">
-          <label data-toggle="tooltip" title="Three-digits code on the back of your card">CVV <i class="fa fa-question-circle"></i></label>
+          <label
+            data-toggle="tooltip"
+            title="Three-digits code on the back of your card"
+            >CVV <i class="fa fa-question-circle"></i
+          ></label>
+           <span style="position:relative;display:inline-block">
           <input
             v-model="cvv"
             @keypress="isNumber($event)"
-            :maxlength="4"
+            :maxlength="3"
             required
             class="form-control"
+            :class="{ 'error': !cvv_valid, 'success' : cvv_valid}"
           />
-        <span class="error_message">{{ cvv_message }}</span>
+           <div class="success-icon" v-if="cvv_valid">
+              <i class="fas fa-check-circle"></i>
+            </div>
+           </span>
         </div>
       </div>
     </div>
-    <div class="col-sm-12" v-if="success_valid">
-      <p style="color:green;font-size:10pt;"> Credit card berhasil di verifikasi </p>
-    </div>
-      <!-- <button v-on:click="$emit('ganti-gambar')">Random gambar</button> -->
 
+    <div class="col-sm-12" v-if="success_valid">
+      <p style="color: green; font-size: 10pt">{{ success_message }}</p>
+      <p style="color: red; font-size: 10pt">{{ error_message }}</p>
+    </div>
+    <!-- <button v-on:click="$emit('ganti-gambar')">Random gambar</button> -->
   </div>
 </template>
-
 
 <script >
 export default {
@@ -81,77 +102,85 @@ export default {
       mm: "",
       yy: "",
       cvv: "",
-      cc_message:"",
-      mm_message:"",
-      yy_message:"",
-      cvv_message:"",
-      nomor_kartu_valid:false,
-      mm_valid:false,
-      yy_valid:false,
-      cvv_valid:false,
-      success_valid:false,
+      nomor_kartu_valid: false,
+      mm_valid: false,
+      yy_valid: false,
+      cvv_valid: false,
+      success_valid: false,
+      success_message: "",
+      error_message: "",
       xendit: window.Xendit,
-      data: ""
+      validMonth: [
+        "01",
+        "02",
+        "03",
+        "04",
+        "05",
+        "06",
+        "07",
+        "08",
+        "09",
+        "10",
+        "11",
+        "12",
+      ],
     };
   },
   watch: {
     nomor_kartu() {
-      // this.cc_message = this.nomor_kartu.length < 16 ? "Harus 16 digit" : "";
-      if(this.nomor_kartu.length < 16)
-      {
-        this.cc_message = "Harus 16 digit"
-        this.nomor_kartu_valid = false
-        this.success_valid=false
-      }
-      else
-      {
-        this.cc_message = ""
-        this.nomor_kartu_valid = true
-        this.checkValidation(this.nomor_kartu_valid,this.mm_valid,this.yy_valid,this.cvv_valid)
+      if (this.nomor_kartu.length < 16) {
+        this.nomor_kartu_valid = false;
+        this.success_valid = false;
+      } else {
+        this.nomor_kartu_valid = true;
+        this.checkValidation(
+          this.nomor_kartu_valid,
+          this.mm_valid,
+          this.yy_valid,
+          this.cvv_valid
+        );
       }
     },
-    mm(){
-      var validMonth = ["01","02","03","04","05","06","07","08","09","10","11","12"]
-      // this.mm_message =  validMonth.indexOf(this.mm) === -1 ? "Bulan tidak valid" : "";
-      if(validMonth.indexOf(this.mm) === -1)
-      {
-        this.mm_message = "Bulan tidak valid"
+    mm() {
+      if (this.validMonth.indexOf(this.mm) === -1) {
         this.mm_valid = false;
-        this.success_valid=false
-      }
-      else
-      {
-        this.mm_message = ""
-        this.mm_valid = true;
-        this.checkValidation(this.nomor_kartu_valid,this.mm_valid,this.yy_valid,this.cvv_valid)
+        this.success_valid = false;
+      } else {
+         this.mm_valid = true;
+        this.checkValidation(
+          this.nomor_kartu_valid,
+          this.mm_valid,
+          this.yy_valid,
+          this.cvv_valid
+        );
       }
     },
-    yy(){
-      if(this.yy.length != 2)
-      {
-        this.yy_message = "Tahun tidak valid"
-        this.yy_valid = false
-        this.success_valid=false
-      }
-      else
-      {
-        this.yy_message = ""
+    yy() {
+      if (this.yy.length != 2) {
+         this.yy_valid = false;
+        this.success_valid = false;
+      } else {
         this.yy_valid = true;
-        this.checkValidation(this.nomor_kartu_valid,this.mm_valid,this.yy_valid,this.cvv_valid)
+        this.checkValidation(
+          this.nomor_kartu_valid,
+          this.mm_valid,
+          this.yy_valid,
+          this.cvv_valid
+        );
       }
     },
-    cvv(){
-      if(this.cvv.length < 3)
-      {
-        this.cvv_message = "CVV tidak valid"
-        this.cvv_valid = false
-        this.success_valid=false
-      }
-      else
-      {
-        this.cvv_message = ""
+    cvv() {
+      if (this.cvv.length < 3) {
+        this.cvv_valid = false;
+        this.success_valid = false;
+      } else {
         this.cvv_valid = true;
-        this.checkValidation(this.nomor_kartu_valid,this.mm_valid,this.yy_valid,this.cvv_valid)
+        this.checkValidation(
+          this.nomor_kartu_valid,
+          this.mm_valid,
+          this.yy_valid,
+          this.cvv_valid
+        );
       }
     },
   },
@@ -162,82 +191,73 @@ export default {
       if (charCode < 48 || charCode > 57) {
         evt.preventDefault();
       } else {
-        // return this.nomor_kartu.replace(/^(.{4})(.{4})(.{4})(.*)$/, "   ");
         return true;
       }
     },
-    createXenditToken: function() {
-        let apiKey = "xnd_public_development_wSNVjEsIZgpKzUuIZWlTwKsWl7aBKUp1rTXeuO55ekUUiDZhhGwHHy0V4dhJ";
-
-        let url = "https://api.xendit.co/v2/credit_card_tokens";
-        var headers = new Headers();
-        headers.append("Authorization", "Basic " + btoa(apiKey + ":"));
-        headers.append("Content-Type", "application/json");
-
-        var reqBody = JSON.stringify({
-            "amount": "10000",        
-            "card_cvn": "123",
-            "card_data" : {
-              "account_number": "4000000000000002",        
-              "cvn" : "123",
-              "exp_month": "12",        
-              "exp_year": "2020",        
-            },   
-            "is_single_use": true,
-            "should_authenticate": false
-        });
-
-        var requestOptions = {
-          method: 'POST',
-          headers: headers,
-          body: reqBody,
-          redirect: 'follow'
+    checkValidation: function (cc, mm, yy, cvv) {
+      if (cc && mm & yy & cvv) {
+        this.xendit.setPublishableKey(
+          "xnd_public_development_wSNVjEsIZgpKzUuIZWlTwKsWl7aBKUp1rTXeuO55ekUUiDZhhGwHHy0V4dhJ"
+        );
+        var tokenData = {
+          amount: "10000",
+          card_number: this.nomor_kartu,
+          card_exp_month: this.mm,
+          card_exp_year: "20" + this.yy,
+          card_cvn: this.cvv,
+          is_multiple_use: false,
+          should_authenticate: false,
         };
-
-        fetch(url, requestOptions)
-          .then(response => response.text())
-          .then(result => console.log(result))
-          .catch(error => console.log('error', error));
-
-        // var headers = new Headers();
-        // console.log(btoa(apiKey + ":"))
-        // headers.append("Authorization", "Basic " + btoa(apiKey + ":"));
-        // headers.append("Content-Type", "application/json");
-        // console.log(headers);
-        // var tokenData = 
-        // {        
-        //     "amount": "10000",        
-        //     "card_number": "4000000000000002",        
-        //     "card_exp_month": "12",        
-        //     "card_exp_year": "2020",        
-        //     "card_cvn": "123",
-        //     "is_multiple_use": false,
-        //     "should_authenticate": true
-        // }
-        // this.xendit.card.createToken(tokenData, function (err, datas) {
-        //   if (err) {
-        //       //Define error handling
-        //   }
-        //   console.log(datas)
-        // });
+        this.xendit.card.createToken(tokenData, this.handleToken);
+      }
     },
-    checkValidation: function (cc,mm,yy,cvv) {
-      
-      if(cc && mm & yy & cvv)
-      {
+    handleToken: function (err, data) {
+      if (err) {
         this.success_valid = true;
-        if(this.success_valid == true)
-        {
-          this.createXenditToken();
+        this.error_message = err.message;
+      } else {
+        if (data.status === "VERIFIED") {
+          this.success_valid = true;
+          localStorage.setItem("tokenResponse", JSON.stringify(data));
+          // var token = JSON.parse(localStorage.getItem("tokenResponse"));
+          this.success_message = "Verifikasi Berhasil";
+          this.error_message = "";
+        } else if (data.status === "IN_REVIEW") {
+          localStorage.setItem("tokenResponse", JSON.stringify(data));
+          // token = JSON.parse(localStorage.getItem("tokenResponse"));
+          this.success_message = "Verifikasi Berhasil";
+          this.error_message = "";
+        } else if (data.status === "FAILED") {
+          console.log(data.id);
+          alert(data.status);
         }
-      } 
-    }
+      }
+    },
   },
 };
 </script>
 <style scoped>
-  .error_message {
-    color:red;
-    font-size:8pt
-  }
+.error_message {
+  color: red;
+  font-size: 8pt;
+}
+.success-icon {
+  position: absolute;
+  right: 5px;
+  top: 6px;
+  color: green;
+  display: inline-block;
+}
+.error:focus-within {
+  border:1px solid red;
+}
+.success:focus-within {
+  border:1px solid green
+}
+.error:focus {
+  box-shadow:0 0 0 0.2rem rgb(253 34 34 / 25%);
+}
+.success:focus {
+  box-shadow: 0 0 0 0.2rem rgb(57 253 34 / 25%);
+}
 </style>
