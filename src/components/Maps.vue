b20d53482e92c37e352de74eab4163a7851ca29c
Ridho Fariha <ridho@majaindo.com>
2020-11-26 14:39:46 +0700

    draggable pin

--- src/components/Maps.vue
+++ src/components/Maps.vue
@@ -8,6 +8,7 @@
 
 
 <script >
+// import func from '../../vue-temp/vue-editor-bridge.js';
 import { eventBus } from '../assets/event-bus.js'
 export default {
   data: function () {
@@ -18,12 +19,23 @@ export default {
     };
   },
   mounted: function() {
-    this.load()
+    if(window.google)
+    {
+      this.load()
+    }
+    else
+    {
+      var self = this;
+      setTimeout(function(){
+        self.load()
+      },1000)
+    }
   },
   methods: {
     load: function () {
+      console.log(window.google)
         this.geocoder = new window.google.maps.Geocoder();
-
+        console.log(this.geocoder)
         this.map = new window.google.maps.Map(this.$refs["map"],{
           center : {
             lat: 3.590000, lng: 98.678020
@@ -36,10 +48,13 @@ export default {
 
         this.placesService = new window.google.maps.places.PlacesService(this.map);
 
+        var latlng = { lat: 3.5899278, lng: 98.6741724 }
+        this.updateMarker(latlng)
+        
         window.google.maps.event.addListener(this.map, "click", (event) => {
         this.lastLat = event.latLng.lat();
         this.lastLng = event.latLng.lng();
-
+        
         this.updateMarker(event.latLng);
         console.log(this.lastLat,this.lastLng);
         this.geocoder.geocode({'location': event.latLng}, (results) => {
@@ -67,8 +82,21 @@ export default {
           map: this.map,
           draggable:true
         });
-      }
 
+        window.google.maps.event.addListener(this.marker, "dragend", (event) => {
+        this.lastLat = event.latLng.lat();
+        this.lastLng = event.latLng.lng();
+        console.log(this.lastLat,this.lastLng)
+        this.geocoder.geocode({'location': event.latLng}, (results) => {
+          if (results) {
+            console.log(results)
+            eventBus.$emit('mapAddress', results);
+          }
+        });
+      });
+      
+      }
+      
       this.marker.setPosition(latLng);
     },
   }
